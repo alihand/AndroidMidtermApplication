d9aa86926c8b0ae2f8907cabb52297fb15d849a4
Alihan Demir <34598775+alihand@users.noreply.github.com>
2019-01-24 12:43:00 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/alihandemir/androidmidterm/MainActivity.java
+++ app/src/main/java/com/example/alihandemir/androidmidterm/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.alihandemir.androidmidterm;
 
-import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
